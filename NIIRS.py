ea4f8563699d752c3c75924422f4ed1746d1f1cf
xandriaw <xandria.webb@gmail.com>
2017-02-27 14:51:23 -0500

    still works
    took out "basic model=model() since this wasn't working anyways.

--- NIIRS.py
+++ NIIRS.py
@@ -20,10 +20,7 @@ mu, sigma = 4.5, 1.5
 
 
 from pymc import TruncatedNormal, HalfNormal, Normal, Model
-
-basic_model = Model()
-with basic_model:
-    mu_dist = TruncatedNormal('mu_dist', mu=mu, tau=sigma, a=a, b=b)
-    sigma_dist = HalfNormal('sigma_dist', tau=1) #use a half-normal since sd is always positive, had sd=1, maybe for pymc3
-    Y_obs= TruncatedNormal('Y_obs', mu=mu_dist, tau=sigma_dist, a=a, b=b) #, observed=True) this was giving error- must have an initial value if observed=True
+mu_dist = TruncatedNormal('mu_dist', mu=mu, tau=sigma, a=a, b=b)
+sigma_dist = HalfNormal('sigma_dist', tau=1) #use a half-normal since sd is always positive, had sd=1, maybe for pymc3
+Y_obs= TruncatedNormal('Y_obs', mu=mu_dist, tau=sigma_dist, a=a, b=b) #, observed=True) this was giving error- must have an initial value if observed=True
     
