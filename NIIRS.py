d15e61ca6cf1eb86517569a35cef87a47372866e
xandriaw <xandria.webb@gmail.com>
2017-03-29 15:59:15 -0400

    plots in pymc2
    got the plot to show truncated normal as i would expect. yay!

--- NIIRS.py
+++ NIIRS.py
@@ -9,6 +9,7 @@ Created on Sat Feb 18 14:02:19 2017
 import numpy as np
 import scipy.stats as stats
 import matplotlib.pyplot as plt
+import matplotlib
 
 np.random.seed(123)
 
@@ -16,11 +17,75 @@ np.random.seed(123)
 lower, upper = 0, 10
 mu, sigma = 4.5, 1.5
 #use a truncated normal random variable
-[a,b] =(lower - mu) / sigma, (upper - mu) / sigma
+###this normalizes our bounds- but i don't think we want this###
+#[a,b] =(lower - mu) / sigma, (upper - mu) / sigma
+###this may not be what we actually want to use as upper and lower
 
+from pymc import TruncatedNormal, HalfNormal, Normal, Model, MCMC, Metropolis
 
-from pymc import TruncatedNormal, HalfNormal, Normal, Model
-mu_dist = TruncatedNormal('mu_dist', mu=mu, tau=sigma, a=a, b=b)
+mu_dist = TruncatedNormal('mu_dist', mu=mu, tau=sigma, a=lower, b=upper)
 sigma_dist = HalfNormal('sigma_dist', tau=1) #use a half-normal since sd is always positive, had sd=1, maybe for pymc3
-Y_obs= TruncatedNormal('Y_obs', mu=mu_dist, tau=sigma_dist, a=a, b=b) #, observed=True) this was giving error- must have an initial value if observed=True
+Y_obs= TruncatedNormal('Y_obs', mu=mu_dist, tau=sigma_dist, a=lower, b=upper) #, observed=True) this was giving error- must have an initial value if observed=True
     
+sim=MCMC([mu_dist, sigma_dist, Y_obs])
+sim.sample(50000, 10000, 1)
+
+y_samples = sim.trace("mu_dist")[:]
+fig = plt.figure(figsize=(5,5))
+axes = fig.add_subplot(111)
+axes.hist(y_samples, bins=50, normed=True, color="gray");
+fig.show()
+
+
+###This attempts the same but with newer version of pymc3. 
+# Doesn't work for bounding distribution
+import pymc3 as pm
+import numpy as np
+# after reading some of the probablistic programming book:
+with pm.Model() as my_model:
+    mu_dist= pm.Normal('mu_dist', mu=mu,sd=sigma   )
+    sigma_dist = pm.HalfNormal('sigma_dist', tau=1)
+    Y_obs= pm.Normal('Y_obs', mu=mu_dist, tau=sigma_dist)
+    step = pm.Metropolis()
+    trace = pm.sample(20000, step=step)
+    burned_trace=trace[1000:]
+    
+mu_samples= burned_trace["mu_dist"]
+sigma_samples = burned_trace["sigma_dist"]
+Y_samples= burned_trace["Y_obs"]
+
+%matplotlib inline
+figsize(12.5, 10)
+
+#histogram of posteriors
+
+ax = plt.subplot(311)
+
+p#lt.xlim(0, .1)
+plt.hist(mu_samples, histtype='stepfilled', bins=25, alpha=0.85, label="posterior of mu", color="#A60628", normed=True)
+plt.vlines(mu, 0, 0.3, linestyle="--", label="true mu (unknown)")
+plt.legend(loc="upper right")
+plt.title("Posterior distributions ")
+
+ax = plt.subplot(312)
+#this one doesn't work
+#plt.xlim(0, .1)
+plt.hist(sigma_samples, histtype='stepfilled', bins=25, alpha=0.85, label="posterior of sigma", color="#467821")
+plt.vlines(sigma, 0, .3, linestyle="--", label="true sigma (unknown)")
+plt.legend(loc="upper right")
+
+ax = plt.subplot(313)
+plt.hist(Y_samples, histtype='stepfilled', bins=30, alpha=0.85, label="posterior of Y-obs", color="#7A68A6", normed=True)
+#plt.vlines(Y_obs, 0, 60, linestyle="--", label="true delta (unknown)")
+#plt.vlines(0, 0, 60, color="black", alpha=0.2)
+plt.legend(loc="upper right");
+
+def bayes_CR_mu(D, sigma, frac=0.95):
+    """Compute the credible region on the mean"""
+    #D is
+    Nsigma = np.sqrt(2) * erfinv(frac)
+    mu = D.mean()
+    sigma_mu = sigma * D.size ** -0.5
+    return mu - Nsigma * sigma_mu, mu + Nsigma * sigma_mu
+
+print("95% Credible Region: [{0:.0f}, {1:.0f}]".format(*bayes_CR_mu(D, 10)))
