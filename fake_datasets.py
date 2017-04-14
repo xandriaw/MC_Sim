a91227e977ffefb43ec68128913a841ed1aa937e
xandriaw <xandria.webb@gmail.com>
2017-04-14 16:43:25 -0400

    plot series
    plots series - one for each possible variance, with sample size on
    x-axis and probability that analyst chooses same as robot on y -axis

--- fake_datasets.py
+++ fake_datasets.py
@@ -15,19 +15,10 @@ def round_to_half(number):
     """Round a number to the closest half integer."""
     return np.around(number * 2) / 2
 
-from scipy.special import erfinv
-
-def bayes_CR_mu(D, sigma, frac=0.95):
-    """Compute the credible region on the mean"""
-    Nsigma = np.sqrt(2) * erfinv(frac)
-    mu = D.mean()
-    sigma_mu = sigma * D.size ** -0.5
-    return mu - Nsigma * sigma_mu, mu + Nsigma * sigma_mu
-
 sampleSize= np.arange(1,100,1)
-variances = np.array([1, .75, .5, .25, .1])
+variances = np.array([ 1, .75, .5, .3, .2, .1])
 #note that tau is not sigm! 
-#sigma^2=1/\tau
+#sigma^2=1/tau
 taus = 1/variances
 
 df=pd.DataFrame(columns = ["variance", "sampleSize","botMean", "humanMean", 
@@ -37,6 +28,7 @@ df=pd.DataFrame(columns = ["variance", "sampleSize","botMean", "humanMean",
 
 
 i=1
+
 for t in taus:
     # the bot will always give the same output for a given image
     botOutput = TruncatedNormal('botOutput', mu=4.5, tau=t, a=1, b=10)
@@ -54,27 +46,24 @@ for t in taus:
         botOutput = np.full(s,4.5+b)
         #vary the human output for this image
         humanOutput = round_to_half(sim.trace("humanOutput")[:])
-#        
-#        fig = plt.figure(figsize=(10,10))
-#        axes = fig.add_subplot(111)
-#        axes.hist(botOutput, bins=20, normed=True, color="blue");
-#        axes.hist(humanOutput, bins=20, normed=True, color="red")
-#        fig.show()
-
+        #fill the dataframe
         df.loc[i]=[1/t, s, botOutput.mean(), humanOutput.mean(), b, 
               sum(botOutput==humanOutput)]
         i=i+1
-print(df)
+    #plot for each variance/tau
+    
 df['probability'] = df.matches/df.sampleSize
+    
+print(df)
 #"Given our observed data, there is a 95% probability that the true 
 # value of μμ falls within CRμCRμ" - Bayesians
+fig, axes = plt.subplots(nrows=1, ncols=len(taus))
+fig.canvas.set_window_title('probability that an analyst would tive this image the same NIIRS')
 
-matplotlib.style.use('ggplot')
-plt.plot( df.sampleSize, df.probability , legend=True )
-
-df.plot(x=sampleSize)
-
-ax = df.plot.scatter(x='sampleSize', y='probability', s=df['variance']*50)
-
+for t in taus:
+    df=df.loc[(df.variance ==1/t)]
+    whichplace = int(np.argwhere(taus==t))
+    tdf.plot.scatter(x='sampleSize', y='probability', ax=axes[whichplace])
+    axes[whichplace].set_title('variance:' + str( 1/t)
 
 
