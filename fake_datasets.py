80a7712de099152e0d3307b6534a810f67efdba5
xandriaw <xandria.webb@gmail.com>
2017-04-14 10:42:24 -0400

    added output to dataframe
    and option for a bias to be added to the bot

--- fake_datasets.py
+++ fake_datasets.py
@@ -14,13 +14,14 @@ import pandas as pd
 def round_to_half(number):
     """Round a number to the closest half integer."""
     return np.around(number * 2) / 2
-sampleSize= [10, 50, 100, 1000, 2000, 3000,4000, 5000]
+sampleSize= [5, 10, 15, 20,25,30]
 variances = np.array([1, .75, .5, .25, .1])
 #note that tau is not sigm! 
 #sigma^2=1/\tau
 taus = 1/variances
 
-df=pd.DataFrame(columns = ["variance", "sampleSize", "Matches"])
+df=pd.DataFrame(columns = ["variance", "sampleSize","botMean", "humanMean", 
+                           "bias","Matches"])
 
 i=1
 for t in taus:
@@ -40,21 +41,21 @@ for t in taus:
         botOutput = np.full(s,4.5+b)
         #vary the human output for this image
         humanOutput = round_to_half(sim.trace("humanOutput")[:])
-        
-        fig = plt.figure(figsize=(10,10))
-        axes = fig.add_subplot(111)
-        axes.hist(botOutput, bins=20, normed=True, color="blue");
-        axes.hist(humanOutput, bins=20, normed=True, color="red")
-        fig.show()
+#        
+#        fig = plt.figure(figsize=(10,10))
+#        axes = fig.add_subplot(111)
+#        axes.hist(botOutput, bins=20, normed=True, color="blue");
+#        axes.hist(humanOutput, bins=20, normed=True, color="red")
+#        fig.show()
     
         #find where they are the same..?
         print()
         print "variance:", 1/t, "sample size: " , s 
         print( )
         print "number of times human and bot are the same:", sum(botOutput ==humanOutput)
-        df.append({"variance": 1/t, "sampleSize": s,"Matches:": sum(botOutput ==humanOutput)}, 
-                   ignore_index=True)
-        df.loc[i]=[1/t, s, sum(botOutput==humanOutput)]
+#        df.append({"variance": 1/t, "sampleSize": s,"Matches:": sum(botOutput ==humanOutput)}, 
+#                   ignore_index=True)
+        df.loc[i]=[1/t, s, botOutput.mean(), humanOutput.mean(), b, sum(botOutput==humanOutput), ]
         i=i+1
 print(df)
 
