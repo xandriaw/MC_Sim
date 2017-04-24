cffc82b53f68ae4f0a5f8eeab52e1115c053f0a3
xandriaw <xandria.webb@gmail.com>
2017-04-24 16:43:26 -0400

    minor corrections
    minor corrections and adding in difference between the two distributions

--- fake_datasets.py
+++ fake_datasets.py
@@ -15,7 +15,7 @@ def round_to_half(number):
     """Round a number to the closest half integer."""
     return np.around(number * 2) / 2
 
-sampleSize= np.arange(1,100,1)
+sampleSize= np.arange(10,1000,1)
 variances = np.array([ 1, .75, .5, .3, .2, .1])
 #note that tau is not sigm! 
 #sigma^2=1/tau
@@ -31,16 +31,17 @@ i=1
 
 for t in taus:
     # the bot will always give the same output for a given image
-    botOutput = TruncatedNormal('botOutput', mu=4.5, tau=t, a=1, b=10)
+    #tau = 1/variance - this is varying for each iteration of the loop
+    placeHolder = TruncatedNormal('placeHolder', mu=4.5, tau=t, a=1, b=10)
     humanOutput = TruncatedNormal('humanOutput', mu=4.5, tau=t, a=1, b=10)
     
-    sim=MCMC([botOutput, humanOutput])
+    sim=MCMC([placeHolder, humanOutput])
 
 
     for s in sampleSize:
         #get s number of samples, no burn in for this- not optimizing anything
         sim.sample(s, 0, 1)
-        #assume no bias at this point
+        #assume no bias at this point but we can add bias later
         b=0
         #computer is always going to get the same value for an image
         botOutput = np.full(s,4.5+b)
@@ -53,17 +54,19 @@ for t in taus:
     #plot for each variance/tau
     
 df['probability'] = df.matches/df.sampleSize
+df['difference']= np.abs(df.botMean-df.humanMean)
     
 print(df)
 #"Given our observed data, there is a 95% probability that the true 
 # value of μμ falls within CRμCRμ" - Bayesians
 fig, axes = plt.subplots(nrows=1, ncols=len(taus))
-fig.canvas.set_window_title('probability that an analyst would tive this image the same NIIRS')
+fig.canvas.set_window_title('probability that an analyst would give this image the same NIIRS')
 
 for t in taus:
     df=df.loc[(df.variance ==1/t)]
     whichplace = int(np.argwhere(taus==t))
     tdf.plot.scatter(x='sampleSize', y='probability', ax=axes[whichplace])
-    axes[whichplace].set_title('variance:' + str( 1/t)
+    axes[whichplace].set_title('variance:' + str( 1/t))
+
 
 
