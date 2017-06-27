bce4569cd302c1c793919ea877e42b6b6078bba0
xandriaw <xandria.webb@gmail.com>
2017-06-27 12:42:43 -0400

    actual functions
    moved things around so that these are now actual functions and work
    individually and as a whole

--- functionizing_fake_data.py
+++ functionizing_fake_data.py
@@ -12,6 +12,14 @@ from pymc import TruncatedNormal, HalfNormal, Normal, Model, MCMC, Metropolis, U
 import pandas as pd
 import math
 
+
+import matplotlib.pyplot as plt
+from matplotlib.transforms import blended_transform_factory
+import matplotlib.lines as mpllines
+import matplotlib.ticker as mticker
+from pymc.distributions import noncentral_t_like
+
+
 def round_to_half(number):
     """Round a number to the closest half integer."""
     return np.around(number * 2) / 2
@@ -41,84 +49,55 @@ def HDIofMCMC(differenceOfMeans, credMass=0.95):
     HDImax=sortedPts[ciWidth.argmin()+ciLength]
     HDIlim = [HDImin, HDImax]
     return HDIlim
-##for testing R and python code
-#differenceOfMeans=np.genfromtxt('C:\\Users\\xandr\\OneDrive\\Documents\\GitHub\\quality_data_science\\sampleVec.csv', delimiter=',')
-    #for testing:
-sampleSize=50
-variance=0.2
-ROPESize=0.5
-strictly=True
-credibleMass=0.95
-humanMean=4.5
 
-def probInROPE(humanMean=4.5, sampleSize=50, variance=0.2, ROPESize=0.5, 
-               strictly=True, credibleMass=0.95):
-#set our variables here
-#ROPE is the Region of practical equivalence. 
-#This is often [-0.1, 0.1] or something like this.
-#should our rope be strictly less than or less than or equal to the limits
-#note that tau is not sigma
-#sigma^2=1/tau
-t = 1/variance
+def differenceOfmeans(humanMean=4.5, sampleSize=50, variance=0.2):
+    #note that tau is not sigma
+    #sigma^2=1/tau
+    t = 1/variance
+    #what is the probability that an analyst would give this image the same rating?
+    mu = TruncatedNormal('mu', mu=humanMean, tau = t, a=1, b=10) #hypothetical ground truth
+    botOutput = TruncatedNormal('botOutput', mu=mu, tau=t, a=1, b=10)
+    humanOutput = TruncatedNormal('humanOutput', mu=mu, tau=t, a=1, b=10)
+    #when we have data from the model we can use this here
+    #like this d = pymc.Binomial(‘d’, n=n, p=theta, value=np.array([0.,1.,3.,5.]), observed=True)
+    
+    sim=MCMC([mu, botOutput, humanOutput])
+    
+    sim.sample(sampleSize, 0, 1)
+    botOutput = sim.trace("botOutput")[:]
+    #if humans only give ratings at the 0.5 interval, not smaller
+    #        humanOutput = round_to_half(sim.trace("humanOutput")[:]) 
+    humanOutput = sim.trace("humanOutput")[:]
+    #difference of the means
+    #but what we care about is the mean of the human output for each image.
+    difference = botOutput-humanOutput.mean()
+    return difference
 
-df=pd.DataFrame(columns = ["variance", "sampleSize","botMean", "humanMean", 
-                           "bias","lowerHDI", "upperHDI","probabilityInROPE"])
-
-#what is the probability that an analyst would give this image the same rating?
-mu = TruncatedNormal('mu', mu=humanMean, tau = t, a=1, b=10) #hypothetical ground truth
-botOutput = TruncatedNormal('botOutput', mu=mu, tau=t, a=1, b=10)
-humanOutput = TruncatedNormal('humanOutput', mu=mu, tau=t, a=1, b=10)
-#when we have data from the model we can use this here
-#like this d = pymc.Binomial(‘d’, n=n, p=theta, value=np.array([0.,1.,3.,5.]), observed=True)
-
-sim=MCMC([mu, botOutput, humanOutput])
-
-sim.sample(sampleSize, 0, 1)
-botOutput = sim.trace("botOutput")[:]
-#if humans only give ratings at the 0.5 interval, not smaller
-#        humanOutput = round_to_half(sim.trace("humanOutput")[:]) 
-humanOutput = sim.trace("humanOutput")[:]
-#difference of the means
-#but what we care about is the mean of the human output for each image.
-difference = botOutput-humanOutput.mean()
-#HighestDensityInterval: where 95% of the differences are? are they close to 0?
-HDI= HDIofMCMC(differenceOfMeans= difference, credMass=credibleMass)
 #ROPE
-if strictly == True:
+def probInROPE(difference, ROPESize=0.5 ):
     ROPE=difference[(difference<=ROPESize)&(difference>=(0-ROPESize))]
     probabilityInROPE = np.round(len(ROPE)*1.0/len(difference)*1.0, decimals=2)
-    print  " \n{:0.0f}".format(probabilityInROPE*100) + "% of our robot outputs are less than or equal to 0.5 away from human output" 
-else:
-    ROPE=difference[(difference<ROPESize)&(difference>(0-ROPESize))]
-    probabilityInROPE = np.round(len(ROPE)*1.0/len(difference)*1.0, decimals=2)
-    print  " \n{:0.0f}".format(probabilityInROPE*100) + "% of our robot outputs are strictly less than .5 away from human output" 
+    print  " \n{:0.0f}".format(probabilityInROPE*100) + "% of our robot outputs are inside the ROPE" 
+    return probabilityInROPE
 
 #"Given our observed data, there is a 95% probability that the true 
 # value of μμ falls within CRμCRμ" - Bayesians
 
-fig.canvas.set_window_title('histogram of difference of means'%credibleMass)
-
-import matplotlib.pyplot as plt
-from matplotlib.transforms import blended_transform_factory
-import matplotlib.lines as mpllines
-import matplotlib.ticker as mticker
-from pymc.distributions import noncentral_t_like
-kwargs = {}
-
-fig, ax = plt.subplots()
-plt.hist(difference, rwidth=0.8,
-                 facecolor='#7cb5ec', edgecolor='none')
-ax.set_title('Difference of bot mean and human mean')
-ax.axvline(x=(0-ROPESize), color='red' )
-ax.axvline(x=(ROPESize), color='red' )
-#plot the HDI as well
-ax.hlines(y=.1,  linewidth=6, color = 'black' , xmin = HDI[0], xmax= HDI[1] )
-ax.set_xlim(round(min(difference))-1 )
-
-####Need to figure out how to round this and put it in annotation
-HDIText="HDI: %s" %np.around(HDI, decimals=1)
-ROPEText="ROPE: [-%s, %s]" %(ROPESize, ROPESize)
-ax.annotate(HDIText, (0.4, 0.05), textcoords='axes fraction', size=20, color='black')
-ax.annotate(ROPEText, (.5, .8), textcoords='axes fraction', size = 20, color = 'red')
-
+def plotROPEHDI(difference, HDI, ROPESize=0.5):
+    fig, ax = plt.subplots()
+    fig.canvas.set_window_title("histogram of difference of means {0:.0f}% HDI" .format(credibleMass*100))
+    plt.hist(difference, rwidth=0.8,
+                     facecolor='#7cb5ec', edgecolor='none')
+    ax.set_title('Difference of bot mean and human mean')
+    ax.axvline(x=(0-ROPESize), color='red' )
+    ax.axvline(x=(ROPESize), color='red' )
+    #plot the HDI as well
+    ax.hlines(y=.1,  linewidth=6, color = 'black' , xmin = HDI[0], xmax= HDI[1] )
+    ax.set_xlim(round(min(difference))-1 )
     
+    ####Need to figure out how to round this and put it in annotation
+    HDIText="HDI: {}" .format(np.around(HDI, decimals=1))
+    ROPEText="ROPE: [-{0}, {1}]" .format(ROPESize, ROPESize)
+    ax.annotate(HDIText, (0.4, 0.05), textcoords='axes fraction', size=20, color='black')
+    ax.annotate(ROPEText, (.5, .8), textcoords='axes fraction', size = 20, color = 'red')
+    return fig
