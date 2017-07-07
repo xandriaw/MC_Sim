bfcc3974a2ab1bb8fc328f52eb372ed91fed7447
xandriaw <xandria.webb@gmail.com>
2017-07-07 11:34:34 -0400

    uses fake_data_creation to build bokeh app
    this is from the fake data creation file with functions now creating an
    app with bokeh

---
+++ diff_of_means_histogram_app.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 27 12:14:08 2017
+
+@author: xandr
+"""
+import numpy as np
+from bokeh.plotting import figure, show, output_file
+from bokeh.charts import Histogram
+from bokeh.models import Span
+from bokeh.models.widgets import Slider, TextInput
+from bokeh.io import curdoc
+from bokeh.layouts import row, widgetbox
+
+#ROPE is the Region of practical equivalence. 
+#This is often [-0.1, 0.1] or something like this.
+execfile('C:\\Users\\xandr\\OneDrive\\Documents\\GitHub\\MC_Sim\\functionizing_fake_data.py')
+
+def update_plot(attrname, old, new):
+    difference= differenceOfmeans(humanMean=humanMeanSlider.value, variance=varianceSlider.value, sampleSize =sampleSlider.value)
+    #HighestDensityInterval: where 95% of the differences are? are they close to 0?
+    HDI= HDIofMCMC(differenceOfMeans= difference, credMass=credibleRegionSlider.value)
+    
+    probInROPE(difference, ROPESize=ROPESlider.value )
+    
+    plotROPEHDI(difference, HDI, ROPESize=ROPESlider.value, credMass=credibleRegionSlider.value)
+
+
+    p1 = figure(width=400, height=400, 
+               title = "histogram of difference of means {0:.0f}% HDI" .format(credibleRegionSlider.value*100))
+    hist,edges =np.histogram(difference)
+    p1.quad(top=hist, bottom=0, left=edges[:-1], right=edges[1:],
+            fill_color="#7cb5ec", line_color="#033649")
+    ROPE1 = Span(location=0-ROPESlider.value, dimension='height', line_color='red', 
+                 line_width=3)
+    ROPE2 = Span(location=ROPESlider.value, dimension='height', line_color='red', 
+                 line_width=3)
+    p1.renderers.extend([ROPE1, ROPE2])
+    p1.line(x=[HDI[0], HDI[1]], y=[3,3], line_color='black', line_width=4, legend ="HDI")
+    return p1
+#show(p1)
+sampleSlider = Slider(title="Sample Size", value=50, start=10, end=10000, step=10)
+varianceSlider = Slider(title="Variance", value=0.2, start=0.05, end=2, step=0.05)
+humanMeanSlider= Slider(title="Human Mean NIIRS", value=4.5, start=1, end=10, step=0.1)
+ROPESlider= Slider(title= "ROPE Size (Region of Practical Equivalence)", value = 0.5, start= 0.05, end = 1, step=0.05)
+credibleRegionSlider= Slider(title = "Credible Mass", value = 0.95, start = 0.75, end = 0.995, step=0.05)
+
+
+
+for w in [sampleSlider, varianceSlider, humanMeanSlider, ROPESlider, credibleRegionSlider]:
+    w.on_change('value', update_plot)
+    
+inputs = widgetbox(sampleSlider, varianceSlider, humanMeanSlider, ROPESlider, credibleRegionSlider)
+
+curdoc().add_root(row(inputs, p1, width=800))
+curdoc().title = "Difference of Means"
+
+####Need to figure out how to round this and put it in annotation
+#HDIText="HDI: {}" .format(np.around(HDI, decimals=1))
+#ROPEText="ROPE: [-{0}, {1}]" .format(ROPESize, ROPESize)
+#ax.annotate(HDIText, (0.4, 0.05), textcoords='axes fraction', size=20, color='black')
+#ax.annotate(ROPEText, (.5, .8), textcoords='axes fraction', size = 20, color = 'red')
+#    
+#    
+#    show(p)
